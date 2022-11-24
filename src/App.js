7f1c964bd50bff2480b49161b95faab561e14745
Facundo DE LOS RIOS <facujofre96@gmail.com>
2022-11-24 00:48:17 -0300

    estructura basica del proyecto + estilos basicos (falta responsive)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+
 import './App.css';
+import logo from './images/bbad-logo.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{
+        width: '100%',
+        height: '100vh',
+        display: 'grid',
+        justifyContent: 'center',
+        // justifyItems: 'center',
+        backgroundColor: '#F4D03F',
+        backgroundImage: 'linear-gradient(132deg, #F4D03F 0%, #16A085 100%)',
+
+      }}>
+        <img src={logo} alt="breaking bad logo" style={{
+          maxWidth: '400px',
+          // minWidth: '320px',
+          placeItem: 'center',
+          placeSelf: 'center'
+        }} />
+        <div style={{
+          backgroundColor: '#fff',
+          maxWidth: '400px',
+          // width: '320px',
+          heigth: '300px',
+          padding: '.75rem',
+          borderRadius: '16px',
+          boxShadow: '0px 10px 15px 5px rgb(0 0 0 / 10%)',
+          margin: 'auto',
+
+        }}>
+          <h3 style={{
+            textAlign: 'center',
+            fontStyle: 'italic',
+          }}> <span style={{
+            fontSize: '30px',
+            fontStyle: 'italic',
+
+          }}>"</span> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit, sunt? <span style={{
+            fontSize: '30px',
+            fontStyle: 'italic',
+
+          }}>"</span></h3>
+          <p style={{
+            textAlign: 'end',
+            paddingRight: '2rem'
+          }} >- Author</p>
+        </div>
+        <button style={{
+              margin: 'auto',
+              width: '200px',
+              height: '40px',
+              fontFamily: "'Solway', serif",
+              border: 'solid 1px',
+        }}>Nueva cita</button>
+      </div>
+    </>
   );
 }
 
